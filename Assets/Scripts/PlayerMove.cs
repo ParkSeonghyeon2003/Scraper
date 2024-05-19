5a198583bfc4f2fea67123290a640185e052a845
박성현 <shpark8267@gmail.com>
2024-05-20 02:30:19 +0900

    캐릭터 구현
    - 달리기, 점프 물리 구현
    - idle, 달리기, 점프시 애니메이션 구현

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -5,20 +5,38 @@ public class PlayerMove : MonoBehaviour
 {
     private Vector2 inputVec;
     private float speed = 10.0f;
+    private float jumpPower = 10.0f;
+
+    private bool isGrounded = false;
 
     private Rigidbody2D rigid;
-    private SpriteRenderer spriter;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
 
     private void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
-        spriter = GetComponent<SpriteRenderer>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Jump();
     }
 
     private void FixedUpdate()
     {
-        Vector2 nextVec = inputVec * speed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = new Vector2(inputVec.x * speed, rigid.velocity.y);
+    }
+
+    private void LateUpdate()
+    {
+        if (inputVec.x != 0) {
+            spriteRenderer.flipX = inputVec.x > 0;
+        }
+
+        animator.SetFloat("Speed", inputVec.magnitude);
     }
 
     private void OnMove(InputValue value)
@@ -26,10 +44,25 @@ public class PlayerMove : MonoBehaviour
         inputVec = value.Get<Vector2>();
     }
 
-    private void LateUpdate()
+    private void Jump()
     {
-        if (inputVec.x != 0) {
-            spriter.flipX = inputVec.x < 0;
+        if (isGrounded) {
+            if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) {
+                rigid.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+                isGrounded = false;
+                animator.SetBool("Jumping", true);
+            }
         }
+        else {
+            return;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (!collision.gameObject.CompareTag("Ground"))
+            return;
+        isGrounded = true;
+        animator.SetBool("Jumping", false);
     }
 }
